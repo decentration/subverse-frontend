21abb717cc64ca251c22323f1f8f7328630a319b
decentration <ramseyajram@me.com>
2023-03-25 11:34:49 +0000

    proxy frontend rejig

--- src/hooks/useProxyCall.ts
+++ src/hooks/useProxyCall.ts
@@ -1,55 +1,57 @@
-import { useCallback, useState } from 'react';
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { web3FromSource } from '@polkadot/extension-dapp';
+import { useCallback, useState, useEffect } from 'react';
 import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
+
 
 export const useProxyCall = (account: InjectedAccountWithMeta | null, onSuccess: () => void) => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [transactionSuccess, setTransactionSuccess] = useState(false);
   const clearError = useCallback(() => setErrorMessage(null), []);
+  const [recipientAccount, setRecipientAccount] = useState<string | null>(null);
 
-  
+  useEffect(() => {
+    async function fetchRecipientAccount() {
+      try {
+        const response = await fetch('/api/recipient-account');
+        const data = await response.json();
+        setRecipientAccount(data.recipientAccount);
+      } catch (error) {
+        console.error('Error fetching recipient account:', error);
+      }
+    }
+
+    fetchRecipientAccount();
+  }, []);
 
   const addProxyCall = useCallback(async () => {
-    if (!account) return;
+    if (!account || !recipientAccount) return;
 
     try {
-      const wsProvider = new WsProvider('wss://soupcan1.jelliedowl.com');
-      const api = await ApiPromise.create({ provider: wsProvider });
-      const injector = await web3FromSource(account.meta.source as string);
-
-      api.setSigner(injector.signer);
-
-      const delay = 2000;
-      const amount = 25 * 1e12;
-      const recipientAccount = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-      const transfer = api.tx.balances.transfer(recipientAccount, amount);
-      const proxyType = 'Any'; // Replace with the desired ProxyType
-      const proxyCall = api.tx.proxy.proxy(account.address, proxyType, transfer);
-      const addProxyCall = api.tx.proxy.addProxy(recipientAccount, proxyType, delay);
-
-
-
-      await new Promise<void>((resolve, reject) => {
-        addProxyCall.signAndSend(account.address, ({ events = [], status }) => {
-          if (status.isFinalized) {
-            console.log('Finalized', status.asFinalized.toHex());
-            setTransactionSuccess(true);
-            onSuccess();
-            resolve();
-          }
-        }).catch((error: Error) => {
-            console.log('Error:', error);
-            setErrorMessage(error.message || 'An error occurred during the transaction.');
-            reject(error);
-          });
+      const response = await fetch('http://localhost:3001/api/add-proxy-call', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          accountAddress: account.address,
+          recipientAccount: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+          proxyType: 'Any',
+        }),
       });
+
+      if (response.ok) {
+        setTransactionSuccess(true);
+        onSuccess();
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.error);
+      }
     } catch (error) {
       console.error('Error:', error);
       setErrorMessage(error.message || 'An error occurred during the transaction.');
     }
-  }, [account, onSuccess]);
+  }, [account, recipientAccount, onSuccess]);
 
   return { addProxyCall, errorMessage, clearError, transactionSuccess };
 };
+
 
